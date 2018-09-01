0980e73b8de56d0e7c4861551a36366cd57d8c16
smashthestate <mojo.sceptic47@gmail.com>
2018-09-01 03:46:03 +0200

    Rating table update with PK constraint

--- eqkillboard-rating-calculator/Db/rating_table.sql
+++ eqkillboard-rating-calculator/Db/rating_table.sql
@@ -1,9 +1,10 @@
-CREATE TABLE djeaa
+CREATE TABLE rating
 (
     character_id integer NOT NULL,
     rating real,
     rd real,
     updated_at timestamp with time zone,
+    PRIMARY KEY (character_id),
     CONSTRAINT character_id FOREIGN KEY (character_id)
         REFERENCES character(id) MATCH SIMPLE
         ON UPDATE NO ACTION
