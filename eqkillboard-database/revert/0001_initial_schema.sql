6e77b484ef194b1dbcbee24a21f3466983274a0e
Niedar <Niedar@gmail.com>
2019-08-13 22:31:47 +0200

    Fix revert script for 001_initial_schema.sql

--- eqkillboard-database/revert/0001_initial_schema.sql
+++ eqkillboard-database/revert/0001_initial_schema.sql
@@ -4,10 +4,15 @@ BEGIN;
 
 DROP TABLE killmail;
 DROP TABLE killmail_raw;
+DROP FUNCTION search_characters(TEXT);
 DROP TABLE character;
+DROP FUNCTION search_guilds(TEXT);
 DROP TABLE guild;
+DROP FUNCTION search_zones(TEXT);
 DROP TABLE zone;
+DROP FUNCTION search_classes(TEXT);
 DROP TABLE class;
 DROP TABLE status_type;
+DROP EXTENSION pg_trgm;
 
 COMMIT;
